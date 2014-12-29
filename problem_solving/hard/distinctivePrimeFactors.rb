4ab5ac678b70b743cb4c8dc86ff86d2404fb22ac
Dennis Charles Hackethal <dennis@generalassemb.ly>
2014-12-29 16:30:42 -0600

    Add hard challenges

---
+++ problem_solving/hard/distinctivePrimeFactors.rb
@@ -0,0 +1,5 @@
+# Write a method that finds an integer's distinctive prime factors and returns
+# a hash with keys being the prime factors, and values the respective number
+# of occurences. For example, the prime factors of 12 are 2 * 2 * 3, so the
+# returned hash should be { 2 => 2, 3 => 1 }. The hash has to be ordered by
+# keys in increasing order.
---
+++ problem_solving/hard/procs.rb
@@ -0,0 +1,6 @@
+# Write a method that takes a number and two procs as parameters. The first
+# proc will multiply the number by two, the second will add five to the
+# result. The 'parent' method will return the result of the operations.
+# First, define the procs, then the method.
+
+# Albeit arbitrary, this is good practice for procs.
